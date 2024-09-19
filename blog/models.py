372d7381d9a8d02fb77a35b88d9e279ee1dcf365
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-09-19 09:05:38 +0000

    add likes, comment_count and best_time field in post

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Drafe"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
@@ -17,6 +17,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="blogpost_likes", blank=True)
+    comment_count = models.IntegerField(default=0)
+    best_time = models.CharField(max_length=100, verbose_name="Best time(month/season)")
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
