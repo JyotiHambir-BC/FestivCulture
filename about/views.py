aee0d9e8920e4845f876ac95f75091430ae126d6
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-09-23 12:45:12 +0000

    add decorative lines in about page

--- about/views.py
+++ about/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import About
 
 # Create your views here.
 def about(request):
-    return render(request, 'about.html')
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request, "about.html",
+        {
+            "about": about,
+        },
+    )
+    
